79e97a36ab71322c5c496299c429b7f97a8cbd37
Zachary01 <448067598@qq.com>
2018-08-29 18:27:58 +0800

    Delete config.js

--- config/config.js
+++
@@ -1,6 +0,0 @@
-const network = "https://rinkeby.infura.io/v3/1c0501c9704f4d99b4b9acb30534d60d";
-const mainMne = "vibrant creek tunnel differ guilt unusual display kit bone father enrich shuffle"
-module.exports = {
-    network,
-    mainMne
-};
